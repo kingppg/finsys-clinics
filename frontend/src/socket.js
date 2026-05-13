1bb11b1b4f33db7ca2f3f0b71a3b83a3db5e9dce
kingppg <paulpgutierrez@gmail.com>
2026-05-13 15:15:26 +0800

    fixed socket from being hardcoded

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,11 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
+
+const SOCKET_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+const socket = io(SOCKET_URL, {
+  transports: ["websocket", "polling"],
+  reconnectionAttempts: 5,
+  reconnectionDelay: 1000,
+});
+
 export default socket;
